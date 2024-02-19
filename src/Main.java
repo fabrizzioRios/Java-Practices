4fc2330651a12896d233642762e0c8a507a9c53c
Fabrizzio Rios <warmachine860@gmail.com>
2024-02-19 17:28:18 -0600

    Hello world practica

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        String saludar = "Hola mundo";
+        System.out.println("Fabrizzio Rios");
+
+        String myString = "Test string, hello how are you?";
+
+        int newNumber = 9;
+
+        for (int i = 1; i <=  newNumber ; i++){
+            System.out.println(myString + " " + i);
+        }
+        boolean myFloat = false;
+        if (!myFloat){
+            System.out.println("My value is true");
+        }
+    }
+}
